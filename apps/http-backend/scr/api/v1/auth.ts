74a05ce7f42a9b19be3fe49c8a090dd5f109084c
AMAN SINGH <2amansingh07@gmail.com>
2025-07-29 23:56:44 +0530

    added signin/signup endpoint

--- apps/http-backend/scr/api/v1/auth.ts
+++ apps/http-backend/scr/api/v1/auth.ts
@@ -1,52 +1,84 @@
+import "dotenv/config";
+import { Router } from "express";
 
-import "dotenv/config"
-import { Router } from "express"
-
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { middleware } from "../../middelware";
-import {signupSchema, signinSchema } from "@repo/common/types" //  Note:- code is working.. don't know why this error is showing...
+import { signinSchema, signupSchema } from "@repo/common/types";
+
+import { prisma } from "@repo/db/client";
+
+const router: Router = Router();
+
+router.post("/signup", async (req, res) => {
+
+  const parseData = signupSchema.safeParse(req.body);
+
+  //Db call
+
+  if (!parseData.success) {
+    return res.json({ message: "incorrect inputs" });
+  }
+
+  try {
 
 
-const router:Router = Router();
+     const userData = await prisma.user.create({
+    data: {
+      username: parseData.data.username,
+      email: parseData.data.email,   /// TODO: hashed password
+      password: parseData.data.password,
+    },
+  });
 
+  res.json({
+    userId: userData.id,
+  })
+    
+  } catch (error) {
+    res.json({message: "user already exists with this email"})
+  }
 
-router.post("/signup",  (req, res)=>{
+ 
+});
+
+router.post("/signin", async (req, res) => {
+ 
+ 
+  const parseData = signinSchema.safeParse(req.body);
+
+  if (!parseData.success) {
+    return res.json({ message: "incorrect inputs" });
+  }
+
+try {
   
-   const data = signupSchema.safeParse(req.body);
-
-   //Db call
-    
-if(!data.success){
-    return res.json({message:"incorrect inputs"})
-}
+    //Db call
 
 
+  // TODO: compare the hashed password..
 
-})
+   const userData = await prisma.user.findFirst({
+    where:{
+      email:parseData.data.email,
+      password:parseData.data.password,
+      
+    }
+   });
 
+   if(!userData){
+    return res.status(403).json({
+      message:"user did not exists.."
+    })
+   }
 
-router.post("/signin", middleware, (req, res)=>{
+   const userId = userData?.id
+
+  const token = jwt.sign({ userId },process.env.JWT_SECRET as string);
+
+  res.json({ message: token });
+} catch (error) {
   
-  const data = signinSchema.safeParse(req.body)
-
-  if(!data.success){
-    return res.json({message:"incorrect inputs"})
 }
+});
 
-
-//Db call
-    
-    const userId  = 1;
-    
- const token = jwt.sign({userId}, process.env.JWT_SECRET as string);
-
- res.json({message: token})
-
-
-
-
-})
-
-
-
-export {router}
+export { router };
--- packages/common/src/schema.ts
+++ packages/common/src/schema.ts
@@ -1,21 +1,21 @@
-import {email, string, z} from "zod"
+import { z} from "zod"
 
 
 
-export const sigupSchema = z.object({
+export const signupSchema = z.object({
     username:z.string().min(3).max(12),
-    email:email(),
-    password:string().min(8)
+    email:z.email(),
+    password:z.string().min(8)
 })
 
 
-export const siginSchema = z.object({
-     email:email(),
-    password:string().min(8)
+export const signinSchema = z.object({
+     email:z.email(),
+    password:z.string().min(8)
 })
 
 
 export const roomSchema = z.object({
-    roomid:string()
+    roomid:z.string()
 })
 
