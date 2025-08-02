925b83ab6f851bb315fff5b2f3c4dacaf926a3ef
AMAN SINGH <2amansingh07@gmail.com>
2025-08-02 22:30:23 +0530

    added ws endpoint

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,16 @@
 import "dotenv/config";
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+  rooms: string[];
+  userId: string;
+  ws: WebSocket;
+}
+
+const users: User[] = [];   // you can use redux or sekliton, which is a good approch...
 
 function checkUser(token: string): string | null {
   const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
@@ -31,7 +39,53 @@ wss.on("connection", (ws, request) => {
 
   const userId = checkUser(token as string); // TODO: fix this error
 
+  if (!userId) {
+    wss.close();
+    return;
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws,
+  });
+
+  console.log(userId);
+
   ws.on("message", (message) => {
-    ws.send("pong");
+    const parsedData = JSON.parse(message as unknown as string); // ex:- "{type:"join_room", roomId: "123"}"
+
+    if (parsedData.type === "join_room") {
+      const user = users.find((x) => x.ws === ws); // TODO: check if the user's roomId exists or not...
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if (parsedData.type === "leave_room") {
+      const user = users.find((x) => x.ws === ws);
+      if (!user) {
+        return;
+      }
+      user.rooms = user?.rooms.filter((x) => x === parsedData.roomId);
+    }
+
+
+    //db call to save chats but i not good approch you should use queue.
+
+    
+
+    if(parsedData.type === "chat"){    // ex:- {type:"chat", roomId:"123", message:"hello world"}
+        const roomId = parsedData.roomId
+        const message = parsedData.message
+
+        users.forEach(user =>{
+            if(user.rooms.includes(roomId)){
+                user.ws.send(JSON.stringify({
+                    type:"chat",
+                    message:message,
+                    roomId
+                }))
+            }
+        })
+    }
   });
 });
